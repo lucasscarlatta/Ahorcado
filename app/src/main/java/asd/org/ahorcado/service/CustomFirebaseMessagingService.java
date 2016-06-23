8f34ae45af43fd89f2a4e365d249df7f2037aea2
lucasscarlatta <lucasscarlatta@gmail.com>
2016-06-24 00:23:23 +0200

    Update imports

--- app/src/main/java/asd/org/ahorcado/service/CustomFirebaseMessagingService.java
+++ app/src/main/java/asd/org/ahorcado/service/CustomFirebaseMessagingService.java
@@ -14,6 +14,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
